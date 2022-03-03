6ee2cf552f9d71b619c33e3b6ad9838f014d9021
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2022-03-03 15:23:49 +0300

    Добавьте файлы проекта.

---
+++ Events/Clock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Clock
+    {
+        public void Parsetime(int _sec)
+        {
+            Console.Clear();
+            int _minute = _sec / 60;
+            int _hour = _minute / 60;
+
+            Console.WriteLine($"{_sec} Seconds");
+            Console.WriteLine($"{_minute} Minutes");
+            Console.WriteLine($"{_hour} Hours");
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Events
+{
+    internal class Prograam
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new();
+            Clock clock = new();
+
+            timer.TimeChangeEvent += clock.Parsetime;
+
+            timer.Start();
+        }
+    }
+}
+
+//Задание
+/*
+ * Два класса
+ * Таймер
+ * Часы
+ * 
+ * В таймере метод Start()
+ * Старт с циклом от 0 до 10000
+ * Еще должен быть Sleep(1000)
+ * Отправить сколько прошло времени
+ * 
+ * В часах метод, который парсит данные таймера
+ * Получает на вход значения, например сколько прошло минут (60/n) и секунд
+ * Еще добавить вывод на экрана час
+ */
